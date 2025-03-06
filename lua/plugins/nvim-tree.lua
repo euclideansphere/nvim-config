1953e6e3416c6c076d39a030bbe96f073b4da599
Austin Hartline <austin.dev@protonmail.com>
2025-03-06 00:14:59 -0600

    leaderF to focus deez nutz

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -4,19 +4,38 @@ return {
 		local function on_attach(bufnr)
 			local api = require("nvim-tree.api")
 
-			local function opts(desc)
-				return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-			end
-
 			api.config.mappings.default_on_attach(bufnr)
 
 			-- your removals and mappings go here
-			vim.keymap.set('n', '<leader>ff', api.tree.find_file, opts('focus file'))
+			vim.keymap.set('n', '<leader>F', function()
+				api.tree.find_file({ open = true, focus = true })
+			end, { noremap = true })
 		end
 
 		require("nvim-tree").setup({
 			view = {
-				width = 30,
+				float = {
+					enable = true,
+					open_win_config = function()
+						local screen_w = vim.opt.columns:get()
+						-- local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+						local w_h = 70
+						local s_h = 42
+						local center_x = (screen_w - w_h) / 2
+						local center_y = ((vim.opt.lines:get() - s_h) / 5) - vim.opt.cmdheight:get()
+						return {
+							border = "none",
+							relative = "editor",
+							row = center_y,
+							col = center_x,
+							width = w_h,
+							height = s_h,
+						}
+					end,
+				},
+				width = function()
+					return math.floor(vim.opt.columns:get() * 5)
+				end,
 			},
 			renderer = {
 				group_empty = false,
