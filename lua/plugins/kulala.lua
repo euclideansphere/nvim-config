5f460b78796232470db4b7119bad0fe4acbef9be
Austin Hartline <austin.dev@protonmail.com>
2025-03-09 11:29:03 -0500

    kulala

---
+++ lua/plugins/kulala.lua
@@ -0,0 +1,5 @@
+return {
+	-- kulala is an http client for .http files (everyone at work uses a vscode plugin for this instead)
+	-- http file spec: https://neovim.getkulala.net/docs/usage/http-file-spec/
+	"mistweaverco/kulala.nvim",
+}
