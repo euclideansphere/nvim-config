18ff62515f82fae8a5b2aca1f5c921ccf13eb6f5
austin hartline <austin.dev@protonmail.com>
2026-05-09 09:07:09 -0500

    missing root markers breaking lsp

--- lsp/elixirls.lua
+++ lsp/elixirls.lua
@@ -1,6 +1,7 @@
 return {
 	cmd = { "elixir-ls" },
 	filetypes = { "elixir", "eelixir", "heex", "surface" },
+	root_markers = { "mix.exs", ".git" },
 	settings = {
 		elixirLS = {
 			dialyzerEnabled = false,
